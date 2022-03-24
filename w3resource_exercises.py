3d8db79973be26bb55c46133c7450a126ff7bd0b
miki <mikinagash1997@gmail.com>
2022-03-24 12:01:33 +0200

    first commit !

---
+++ w3resource_exercises.py
@@ -0,0 +1,19 @@
+ #Q1
+# print("twinkle,twinkle,little star, \n \t how i wonder what you are ! \n \t\t up above the world so high,\n\t\t like a dimond in the sky.\n twinkle , twinkle , little star,\n \t how i wonder what you are ")
+
+#q2
+# import sys
+# print("python version")
+# print(sys.version)
+# print("version info")
+# print(sys.version_info)
+
+#q3
+# import datetime
+# now = datetime.datetime.now()
+# print("current date and time :")
+# print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+#q4
+# radius = float(input("enter radius of circle :"))
+# print( "the radius of the circle is :",round(3.14124* radius **2,2) )
